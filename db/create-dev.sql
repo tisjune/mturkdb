1ee29b2a30a93e702abad679d800bb42d9f7c741
Justine Zhang <justine@lyra.stanford.edu>
2015-05-09 01:49:53 -0700

    form uploading and csv parsing

--- db/create-dev.sql
+++ db/create-dev.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Workers;
 DROP TABLE IF EXISTS WorkerAttrs;
 DROP TABLE IF EXISTS Attrs;
+DROP TABLE IF EXISTS Actions;
 
 CREATE TABLE Users (
 	id INTEGER PRIMARY KEY,
@@ -20,17 +21,16 @@ CREATE TABLE Workers (
 
 CREATE TABLE WorkerAttrs (
 	workerid TEXT,
-	attrid INTEGER,
+	amtid INTEGER,
 	value INT,
 	granted INT,
-	PRIMARY KEY (workerid, attrid),
-	FOREIGN KEY(attrid) REFERENCES Attrs(attrid),
+	PRIMARY KEY (workerid, amtid),
+	FOREIGN KEY(amtid) REFERENCES Attrs(amtid),
 	FOREIGN KEY(workerid) REFERENCES Workers(workerid)
 );
 
 CREATE TABLE Attrs (
-	attrid INTEGER PRIMARY KEY,
-	amtid TEXT UNIQUE,
+	amtid TEXT PRIMARY KEY,
 	publicname TEXT,
 	privatename TEXT,
 	publicdescr TEXT,
@@ -43,4 +43,4 @@ CREATE TABLE Actions (
 	descr TEXT,
 	FOREIGN KEY(userid) REFERENCES Users(id)
 );
-INSERT INTO Users VALUES (0, 'lsr', 'lsrmturkdb@gmail.com', 'pbkdf2:sha1:1000$Wcblxhmo$abefc7a7715ebb01278fc94fee89fb46c2702c43', 1, 'LSR admin', null, null);
+INSERT INTO Users VALUES (0, 'Admin', 'lsrmturkdb@gmail.com', 'pbkdf2:sha1:1000$Wcblxhmo$abefc7a7715ebb01278fc94fee89fb46c2702c43', 1, 'LSR admin', null, null);
